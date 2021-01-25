63f052657ad324a397beb47bf1d9b40df8005fa9
unknown <wujie_dlut@163.com>
2021-01-25 14:07:28 +0800

    absa

--- absa_main.py
+++ absa_main.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
     # epochs = [1]
     epochs = [3]
     # batch_sizes = [10]
-    batch_sizes = [96]
+    batch_sizes = [70]
     times = 5  # 设置为1是为了测试看结果
     print("training times = ", times)
     model_name = "FuzzyBertCNNBiGRUModel_multiGPU_20210111"
@@ -374,7 +374,7 @@ if __name__ == "__main__":
         review_sentiment_membership_degree_train = dp.getMembershipDegrees(membership_degree_path_train)
         membership_degree_path_validation = config.membership_degree_validation
         review_sentiment_membership_degree_validation = dp.getMembershipDegrees(membership_degree_path_validation)
-        filters = [256, 512, 1024]
+        filters = [512, 1024]
         window_sizes = [4]
         gru_output_dim_1 = [128, 256, 512]
         for cnn_filter in filters:
